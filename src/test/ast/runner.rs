bcb2c476235240e5a5d29ce2f617661d4950db2f
Emmie Maeda <emmie.maeda@gmail.com>
2025-05-25 04:05:45 -0400

    Move comment notes to respective files, add debug test.

--- src/test/ast/runner.rs
+++ src/test/ast/runner.rs
@@ -33,3 +33,99 @@ impl Test {
         todo!()
     }
 }
+
+/*
+     XXX
+impl Test {
+    pub fn run(&self) -> TestResult {
+        if SKIP_TESTS.contains(&&*self.name) {
+            println!("+ {} [SKIPPED]", self.name);
+            return TestResult::Skip;
+        }
+
+        if !ONLY_TESTS.is_empty() && only_test_should_skip(&&*self.name) {
+            println!("+ {} [SKIPPED]", self.name);
+            return TestResult::Skip;
+        }
+
+        debug!(
+            "Running syntax tree test case {} on {}",
+            &self.name, &self.input,
+        );
+
+        println!("+ {}", self.name);
+
+        let page_info = PageInfo {
+            page: Cow::Owned(format!("page-{}", self.name)),
+            category: None,
+            site: cow!("test"),
+            title: cow!(self.name),
+            alt_title: None,
+            score: ScoreValue::Integer(0),
+            tags: vec![cow!("fruit"), cow!("component")],
+            language: cow!("default"),
+        };
+
+        let settings = WikitextSettings::from_mode(WikitextMode::Page, Layout::Wikidot);
+
+        let (mut text, _pages) =
+            crate::include(&self.input, &settings, TestIncluder, || unreachable!())
+                .unwrap_or_else(|x| match x {});
+
+        crate::preprocess(&mut text);
+        let tokens = crate::tokenize(&text);
+        let result = crate::parse(&tokens, &page_info, &settings);
+        let (mut tree, errors) = result.into();
+        tree.wikitext_len = self.tree.wikitext_len; // not stored in the JSON
+        let html_output = HtmlRender.render(&tree, &page_info, &settings);
+
+        fn json<T>(object: &T) -> String
+        where
+            T: serde::Serialize,
+        {
+            let mut output = serde_json::to_string_pretty(object)
+                .expect("Unable to serialize JSON to stdout");
+
+            output.insert_str(0, "Generated JSON: ");
+            output
+        }
+
+        let mut result = TestResult::Pass;
+
+        if tree != self.tree {
+            result = TestResult::Fail;
+            eprintln!(
+                "AST did not match:\nExpected: {:#?}\nActual: {:#?}\n{}\nErrors: {:#?}",
+                self.tree,
+                tree,
+                json(&tree),
+                &errors,
+            );
+        }
+
+        if errors != self.errors {
+            result = TestResult::Fail;
+            eprintln!(
+                "Errors did not match:\nExpected: {:#?}\nActual:   {:#?}\n{}\nTree (for reference): {:#?}",
+                self.errors,
+                errors,
+                json(&errors),
+                &tree,
+            );
+        }
+
+        if html_output.body != self.html {
+            result = TestResult::Fail;
+            eprintln!(
+                "HTML does not match:\nExpected: {:?}\nActual:   {:?}\n\n{}\n\nTree (for reference): {:#?}",
+                self.html,
+                html_output.body,
+                html_output.body,
+                &tree,
+            );
+        }
+
+        result
+    }
+}
+*/
