04b9c0eb76c3941239639cf01a66c7437299490a
jsthope <jsthope@users.noreply.github.com>
2026-03-23 05:01:06 +0100

    add format class for Wikijump date client side rendering

--- src/render/html/element/date.rs
+++ src/render/html/element/date.rs
@@ -33,7 +33,9 @@ pub fn render_date(
         Layout::Wikidot => {
             render_date_wikidot(ctx, date, date_format, hover, &formatted_datetime)
         }
-        Layout::Wikijump => render_date_wikijump(ctx, date, hover, &formatted_datetime),
+        Layout::Wikijump => {
+            render_date_wikijump(ctx, date, date_format, hover, &formatted_datetime)
+        }
     }
 }
 
@@ -46,11 +48,7 @@ fn render_date_wikidot(
 ) {
     let timestamp = date.timestamp();
     let mut class = format!("odate time_{timestamp}");
-
-    if let Some(date_format) = date_format {
-        class.push_str(" format_");
-        class.push_str(&encode_wikidot_date_format(date_format));
-    }
+    push_date_format_class(&mut class, date_format);
 
     ctx.html()
         .span()
@@ -64,28 +62,38 @@ fn render_date_wikidot(
 fn render_date_wikijump(
     ctx: &mut HtmlContext,
     date: DateItem,
+    date_format: Option<&str>,
     hover: bool,
     formatted_datetime: &str,
 ) {
     let timestamp = str!(date.timestamp());
     let delta = str!(date.time_since());
-    let (space, hover_class) = if hover {
-        (" ", "wj-date-hover")
-    } else {
-        ("", "")
-    };
+    let mut class = String::from("wj-date");
+
+    if hover {
+        class.push_str(" wj-date-hover");
+    }
+
+    push_date_format_class(&mut class, date_format);
 
     ctx.html()
         .span()
         .attr(attr!(
-            "class" => "wj-date" space hover_class,
+            "class" => &class,
             "data-timestamp" => &timestamp,
             "data-delta" => &delta,
         ))
         .contents(formatted_datetime);
 }
 
-fn encode_wikidot_date_format(date_format: &str) -> String {
+fn push_date_format_class(class: &mut String, date_format: Option<&str>) {
+    if let Some(date_format) = date_format {
+        class.push_str(" format_");
+        class.push_str(&encode_date_format(date_format));
+    }
+}
+
+fn encode_date_format(date_format: &str) -> String {
     let mut encoded = String::new();
     for byte in date_format.bytes() {
         match byte {
@@ -103,9 +111,22 @@ fn encode_wikidot_date_format(date_format: &str) -> String {
 }
 
 #[test]
-fn wikidot_date_format_encoding() {
-    assert_eq!(
-        encode_wikidot_date_format("%d. %m. %Y"),
-        "%25d.%20%25m.%20%25Y"
-    );
+fn date_format_encoding() {
+    assert_eq!(encode_date_format("%d. %m. %Y"), "%25d.%20%25m.%20%25Y");
+}
+
+#[test]
+fn wikidot_date_class_includes_format() {
+    let mut class = String::from("odate time_1216153821");
+    push_date_format_class(&mut class, Some("%d. %m. %Y"));
+
+    assert_eq!(class, "odate time_1216153821 format_%25d.%20%25m.%20%25Y");
+}
+
+#[test]
+fn wikijump_date_class_includes_format() {
+    let mut class = String::from("wj-date");
+    push_date_format_class(&mut class, Some("%d. %m. %Y"));
+
+    assert_eq!(class, "wj-date format_%25d.%20%25m.%20%25Y");
 }
